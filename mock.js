6dc1cd1cdfd53ec3daf0f712cdae903b4f427495
01mb <91267011+01mb@users.noreply.github.com>
2024-09-14 20:30:13 +0530

    Update mock.js

--- mock.js
+++ mock.js
@@ -1,4 +1,5 @@
 {
+{
     id: "1",
     name: "Pizza Hut",
     dish: "Pizza, Burger, Rolls",
@@ -13,4 +14,5 @@
     rating: "3",
     time: "40",
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUV93qIdDkOSuBIQr1kZEJR9Of2WqVs_L_iQ&s"
+}
 }
